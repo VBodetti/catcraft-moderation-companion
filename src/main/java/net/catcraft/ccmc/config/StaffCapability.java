52a9e03758380a168224457557615c89c571eac9
B0_Deadly <38920796+VBodetti@users.noreply.github.com>
2026-09-19 16:31:36 -0400

    Add staff capability model

---
+++ src/main/java/net/catcraft/ccmc/config/StaffCapability.java
@@ -0,0 +1,13 @@
+package net.catcraft.ccmc.config;
+
+public enum StaffCapability {
+    STAFF_CHAT,
+    BASIC_MODERATION,
+    PLAYER_TELEPORT,
+    TEMP_BAN,
+    ADVANCED_INVESTIGATION,
+    MODERATOR_TOOLS,
+    SENIOR_TOOLS,
+    ADMIN_TOOLS,
+    DEVELOPER_TOOLS
+}
